8cb2f5e6bba12c2d3c6194ca3a4c7b14a00dbb58
Jomana <94597043+Jomjom1114@users.noreply.github.com>
2022-11-06 03:21:00 +0300

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+model.load_model('model.json')
+
+#Caching the model for faster loading
+@st.cache
+
+
+
+
+st.title('Diamond Price Predictor')
+st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
+st.header('Enter the characteristics of the diamond:')
+
