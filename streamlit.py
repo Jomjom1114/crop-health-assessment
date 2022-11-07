7d992244e6e6c742a4275e1cdd3445f1aff778ca
Jomana <94597043+Jomjom1114@users.noreply.github.com>
2022-11-08 01:46:17 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,15 +1,40 @@
 import streamlit as st
-import pandas as pd
+from keras.models import load_model
+from skimage import transform
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+import tensorflow as tf
+import pickle
 
-model.load_model('model.json')
-
-#Caching the model for faster loading
-@st.cache
+model = load_model('model3.h5')
 
 
+def pred(image):
+   st.image(image)
+   shape=((128,128,3))
+   test_image = image.resize((128,128))
+   test_image= tf.keras.preprocessing.image.img_to_array(test_image)
+   test_image= test_image/255.0
+   test_image=np.expand_dims(test_image, axis=0)
+   predictions=model.predict(test_image)
+   st.write(predictions[0])
+   return predictions[0]
 
+def main():
+    st.title('Crop Health Assessment Web App')
 
-st.title('Diamond Price Predictor')
-st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
-st.header('Enter the characteristics of the diamond:')
+    file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
+    image = Image.open(file)
 
+    if st.button('Crop Health Assessment Result'):
+       result = pred(image)
+
+       #st.write(result)
+       if (result > 0.5):
+         st.write('Diseased')
+       else:
+         st.write('Healthy')
+
+if __name__ == '__main__':
+    main()
